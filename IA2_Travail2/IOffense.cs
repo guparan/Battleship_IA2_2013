60fb7c90497dc2a5856e18e1bb132579a291a851
guparan <guparan@15009cc5-d2b0-3236-c39b-4b38fa63c631>
2013-03-25 16:54:05 +0000

    Ajout du Dreadnought

---
+++ IA2_Travail2/IOffense.cs
@@ -0,0 +1,13 @@
+namespace Battleship {
+  using System;
+  using System.Drawing;
+  
+  public interface IOffense {
+    void startGame(int[] ship_sizes);
+    Point getShot();
+    void shotMiss(Point p);
+    void shotHit(Point p);
+    void shotSunk(Point p);
+    void endGame();
+  }
+}
---
+++ IA2_Travail2/SeaState.cs
@@ -0,0 +1,8 @@
+namespace Battleship {
+  public enum SeaState {
+    CLEAR = 0,
+    MISS = 1,
+    HIT = 2,
+    SUNK = 3,
+  }
+}
