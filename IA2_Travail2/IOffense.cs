287afb45e053e7aeb99b287fae868e4538e1af81
guparan <guparan@15009cc5-d2b0-3236-c39b-4b38fa63c631>
2013-04-20 23:16:41 +0000

    Indentation

--- IA2_Travail2/IOffense.cs
+++ IA2_Travail2/IOffense.cs
@@ -1,13 +1,20 @@
-namespace Battleship {
-  using System;
-  using System.Drawing;
-  
-  public interface IOffense {
-    void startGame(int[] ship_sizes);
-    Point getShot();
-    void shotMiss(Point p);
-    void shotHit(Point p);
-    void shotSunk(Point p);
-    void endGame();
-  }
+namespace Battleship
+{
+using System;
+	using System.Drawing;
+
+	public interface IOffense
+	{
+		void startGame (int[] ship_sizes);
+
+		Point getShot ();
+
+		void shotMiss (Point p);
+
+		void shotHit (Point p);
+
+		void shotSunk (Point p);
+
+		void endGame ();
+	}
 }
--- IA2_Travail2/SeaState.cs
+++ IA2_Travail2/SeaState.cs
@@ -1,8 +1,10 @@
-namespace Battleship {
-  public enum SeaState {
-    CLEAR = 0,
-    MISS = 1,
-    HIT = 2,
-    SUNK = 3,
-  }
+namespace Battleship
+{
+	public enum SeaState
+	{
+		CLEAR = 0,
+		MISS = 1,
+		HIT = 2,
+		SUNK = 3,
+	}
 }
