6753fcbdfd9c5683acd1276fbebfc196ec55a5af
LucasSaurel <LucasSaurel@15009cc5-d2b0-3236-c39b-4b38fa63c631>
2013-04-21 16:13:36 +0000

    Commentaires pour Program.cs et RandomOpponent.cs

--- trunk/IA2_Travail2/Program.cs
+++ trunk/IA2_Travail2/Program.cs
@@ -8,26 +8,30 @@
 	{
 		static void Main (string[] args)
 		{
+			// Instanciation des deux joueurs
 			var op1 = new Dreadnought ();
 			var op2 = new RandomOpponent ();
 
+			// Instanciation de la compétition
 			BattleshipCompetition bc = new BattleshipCompetition (
                 op1,
                 op2,
-                new TimeSpan (0, 0, 1),  // Time per game
-                1,                     // Wins per match
-                true,                   // Play out?
-                new Size (10, 10),       // Board Size
-                2, 3, 3, 4, 5           // Ship Sizes
+                new TimeSpan (0, 0, 1),  // Temps limite par jeu
+                1,                       // Nombre de parties gagnantes nécessaires
+                true,                    // Jouer tous les matchs ?
+                new Size (10, 10),       // Taille de la grille
+                2, 3, 3, 4, 5            // Liste des tailles des bâteaux
 			);
 
+			// Lancement de la compétition
 			var scores = bc.RunCompetition ();
 
+			// Affichage des scores
 			foreach (var key in scores.Keys.OrderByDescending(k => scores[k])) {
 				Console.WriteLine ("{0} {1}:\t{2}", key.Name, key.Version, scores [key]);
 			}
 
-			Console.ReadKey (true);
+			Console.ReadKey(true);
 		}
 	}
 }
